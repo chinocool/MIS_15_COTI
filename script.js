41f75089b3de118f2bb79cc87227fa8c8f710631
chinocool <agustinatencio@gmail.com>
2026-05-05 11:31:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,21 +27,32 @@ window.enter = function(withMusic){
   }
 }
 
-// 🔥 SLIDER REAL FUNCIONANDO
-let index = 0;
+// COUNTDOWN
+const target = new Date("July 11, 2026 21:00:00").getTime();
 
 setInterval(()=>{
+  const now = new Date().getTime();
+  const diff = target-now;
+
+  const d = Math.floor(diff/(1000*60*60*24));
+  const h = Math.floor((diff/(1000*60*60))%24);
+  const m = Math.floor((diff/(1000*60))%60);
+
+  document.getElementById("countdown").innerHTML =
+    `${d} días ${h} hs ${m} min`;
+},1000);
+
+// SLIDER
+let i=0;
+setInterval(()=>{
   const track = document.getElementById("track");
-  const images = track.children.length;
+  const total = track.children.length;
 
-  index++;
+  i++;
+  if(i >= total) i = 0;
 
-  if(index >= images){
-    index = 0;
-  }
+  track.style.transform = `translateX(-${i*270}px)`;
 
-  track.style.transform = `translateX(-${index * 300}px)`;
-
-},2500);
+},3000);
 
 });
